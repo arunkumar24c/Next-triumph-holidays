b450b3aa45d985094d3513d46e231252015ee2cb
arunkumar24c <arunkumar24c@gmail.com>
2023-12-15 13:04:34 +0530

    update

--- app/intertour/component/Banner.js
+++ app/intertour/component/Banner.js
@@ -23,12 +23,8 @@ const Banner = () => {
         <div className="brand-container  px-5 py-16 mt-[6rem]">
           <div className="flex flex-col gap-10 justify-center items-center py-5">
             <div>
-             
               <h1 className="text-[35px] font-bold xl:text-[50px] lg:text-[45px] md:text-[40px] text-[#E8FF5B] relative inline-block overflow-hidden px-5">
-                <span className="typing-text block whitespace-nowrap overflow-hidden px-2">
-                  {texts[textIndex]}
-                  <span className="cursor w-[1px] inline-block bg-orange-500 h-[1em] absolute top-0 animate-blink" />
-                </span>
+                Coupon Packages
               </h1>
               <p className="text-[20px] text-white leading-normal text-center font-medium">
                 Your portal to extraordinary marvels around the globe.
